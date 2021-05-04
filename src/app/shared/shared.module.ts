69adf8ed8d7a3075e0a2dbd63b0000668645ff4b
Breens-Thomas <mbakabreens@gmail.com>
2021-05-04 11:33:27 +0300

    INitializing project

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule, MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    RouterModule,
+    HighchartsChartModule
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
